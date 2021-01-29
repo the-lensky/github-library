89a1638eeafe04bba70e1610ef1b20efbdc5b027
Nick <studyaga@mail.ru>
2021-01-29 11:18:31 +0300

    -add finish app

--- src/App.js
+++ src/App.js
@@ -1,15 +1,27 @@
 import React from 'react'
 import './app.scss'
-import {BrowserRouter, Route} from 'react-router-dom'
+import {useDispatch, useSelector} from 'react-redux'
+import {BrowserRouter, Route, Switch, Redirect} from 'react-router-dom'
 import Main from './components/main/Main'
+import Card from './components/card/Card'
+import Error from './components/error/Error'
 
 const App = () => {
+    const dispatch = useDispatch()
+
+
     return (
         <BrowserRouter>
             <div className="container">
-                <Route path='/' component={Main}/>
+                <Switch>
+                    <Route exact path="/" component={Main}/>
+                    <Route path="/card/:username/:reponame" component={Card}/>
+                    <Route path="/error" component={Error}/>
+                    <Redirect to="/"/>
+                </Switch>
             </div>
         </BrowserRouter>
     )
 }
+
 export default App
--- src/components/main/Main.js
+++ src/components/main/Main.js
@@ -1,52 +1,65 @@
-/* eslint-disable no-unused-expressions */
 import React, {useEffect, useState} from 'react'
 import './main.scss'
 import {useDispatch, useSelector} from 'react-redux'
-import {getRepos} from '../../actions/repos'
+import {getRepos} from '../actions/repos'
 import Repo from '../repo/Repo'
+import {setCurrentPage} from '../../reducers/reposReducer'
+import {createPages} from '../../utils/pageCreator'
+import {Redirect} from 'react-router-dom'
 
 const Main = () => {
     const dispatch = useDispatch()
     const repos = useSelector(state => state.repos.items)
-    const isFetching = useSelector(state => state.repos.isFethcing)
+    const isFetching = useSelector(state => state.repos.isFetching)
+    const currentPage = useSelector(state => state.repos.currentPage)
+    const totalCount = useSelector(state => state.repos.totalCount)
+    const perPage = useSelector(state => state.repos.perPage)
+    const isFetchError = useSelector(state => state.repos.isFetchError)
     const [searchValue, setSearchValue] = useState('')
-
+    const pagesCount = Math.ceil(totalCount / perPage)
+    const pages = []
+    createPages(pages, pagesCount, currentPage)
 
     useEffect(() => {
-        dispatch(getRepos())
-            , []
-    })
+        dispatch(getRepos(searchValue, currentPage, perPage))
+    }, [currentPage])
 
-
-    const searchHandler = () => {
-        dispatch(getRepos(searchValue))
+    function searchHandler() {
+        dispatch(setCurrentPage(1))
+        dispatch(getRepos(searchValue, currentPage, perPage))
     }
+
 
     return (
         <div>
-            <div className='search'>
-                <input
-                    type='text'
-                    value={searchValue}
-                    onChange={(e) => setSearchValue(e.target.value)}
-                    className='search-input'
-                />
-                <button
-                    onClick={() => searchHandler()}
-                    className='search-btn'>Search
-                </button>
+            {isFetchError &&
+            <div className="alert alert-danger" role="alert">
+                Произошла ошибка! ПОжалуйста обновите страницу!
             </div>
-            {isFetching === false
-                ?
-                repos.map((repo) => {
-                    return <Repo repo={repo}/>
-                })
-                :
-                <div className='fethcing'>
-                </div>
             }
+            <div className="search">
+                <input value={searchValue} onChange={(e) => setSearchValue(e.target.value)} type="text"
+                       placeholder="Input repo name" className="search-input"/>
+                <button onClick={() => searchHandler()} className="search-btn">Search</button>
+            </div>
+
+            {
+                isFetching === false
+                    ?
+                    repos.map(repo => <Repo repo={repo}/>)
+                    :
+                    <div className="fetching">
+
+                    </div>
+            }
+
+            <div className="pages">
+                {pages.map((page, index) => <span
+                    key={index}
+                    className={currentPage == page ? 'current-page' : 'page'}
+                    onClick={() => dispatch(setCurrentPage(page))}>{page}</span>)}
+            </div>
         </div>
     )
 }
-
 export default Main
